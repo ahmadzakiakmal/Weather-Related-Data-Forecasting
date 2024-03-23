8b6032dc195d1b23baf6682b5957ed57790e695d
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-03-23 13:41:30 +0700

    feat: add code to extract data from specific city

---
+++ data/filter.py
@@ -0,0 +1,14 @@
+import csv
+
+def copy_csv(input_file, output_file, city):
+    with open(input_file, 'r') as infile:
+        reader = csv.reader(infile)
+        with open(output_file, 'w', newline="") as outfile:
+          writer = csv.writer(outfile)
+          for row in reader:
+            if row[1] == city:
+              writer.writerow(row)             
+
+city = input("Enter city name: ")
+copy_csv("./data/join.csv", "./data/" + city + ".csv", city)
+
