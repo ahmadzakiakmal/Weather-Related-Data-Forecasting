5c53a662777357a891f0aa8e29384b9b0b05fe0d
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-03-24 10:41:54 +0700

    feat: done quantitative time series method

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Made as an assignment for Industrial Management Class.
 - All data owned by [Badan Meterologi, Klimatologi, dan Geofisika (BMKG)](https://bmkg.go.id).
 - API by [ibnux](https://github.com/ibnux)
 - [API scraper](https://github.com/ahmadzaki2975/ETL-Pipeline) by me.
+
+# Quantitative Time Series Method
+
+<img src="./images/QuantitativeTimeSeries.png" width="500">
---
+++ src/1-quantitative-time-series.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from matplotlib import dates as mdates
+
+df = pd.read_csv('./data/filtered/Yogyakarta.csv')
+
+df['datetime'] = pd.to_datetime(df.iloc[:, 2])
+df.sort_values('datetime', inplace=True)
+df.set_index('datetime', inplace=True)
+
+rolling_window = 4
+df['temperature'] = df.iloc[:, 6]
+print(df['temperature'])
+df['rolling_avg'] = df['temperature'].rolling(window=rolling_window, center=False).mean()
+
+plt.figure(figsize=(14, 7))
+plt.plot(df.index, df['temperature'], alpha=0.5, label='Actual Temperature')
+
+plt.plot(df.index, df['rolling_avg'], color='red', label='Rolling Average (4 measurements)')
+
+plt.title('Temperature Trend with Rolling Average in Yogyakarta')
+plt.xlabel('Time')
+plt.ylabel('Temperature (°C)')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.gcf().autofmt_xdate() 
+
+plt.show()
